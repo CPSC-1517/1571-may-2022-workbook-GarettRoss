e0ef4603aca20ab93494b80d19ff48d692b18bb9
GarettRoss <garettross@hotmail.com>
2022-05-12 07:59:49 -0600

    Start of Employment Class

---
+++ OOPsReviewSolution/OOPsReview/Employment.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsReview.Data
+{
+    public class Employment
+    {
+        // an instance of this class will hold data about a person's employment
+        //The code of this class is the definition of this data
+        //The characteristics (data) of the class will be the following
+        //Title, SupervisoryLevel, Years of employment within the company
+
+        //there are 4 components of a class definition
+
+        //data fields (data members)
+        //property
+        //constructor
+        // behavior (method)
+    
+        //data fields
+        // are storage areas in your class
+        // these are treated as variables
+        // these may be public, private, public read-only
+
+        //property
+        //These are access techniques to retrieve or set data in your class without directly touching
+        // the storage data field
+    
+    }
+}
